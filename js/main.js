2746add2c883881db7257e940dec55f2f69ffa6e
BPBall <75284072+BPBall@users.noreply.github.com>
2020-12-09 13:47:53 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+/* font-family: 'Kaushan Script', cursive; */
+/* font-family: 'Pinyon Script', cursive; */
+/* font-family: 'Petit Formal Script', cursive; */
+/* font-family: 'Aguafina Script', cursive; */
+/* font-family: 'Rouge Script', cursive; */
+// font-family: 'Roboto', sans-serif;
 
 $(function () {
 	$('a.embed').on('click', function () {
