2adf5c964e7f4540b538b78c469cdcb97fbfec50
BPBall <75284072+BPBall@users.noreply.github.com>
2020-12-09 17:43:58 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
- font-family: 'Kaushan Script', cursive; 
- font-family: 'Pinyon Script', cursive; 
- font-family: 'Petit Formal Script', cursive; 
- font-family: 'Aguafina Script', cursive; 
-font-family: 'Rouge Script', cursive; 
- font-family: 'Roboto', sans-serif;
-
 $(function () {
 	$('a.embed').on('click', function () {
 		$(this.parentNode).children('.embed-pane').toggle();
@@ -14,3 +7,10 @@ $(function () {
     $(this).select();
 	});
 });
+
+font-family: 'Kaushan Script', cursive; 
+font-family: 'Pinyon Script', cursive; 
+font-family: 'Petit Formal Script', cursive; 
+font-family: 'Aguafina Script', cursive; 
+font-family: 'Rouge Script', cursive; 
+font-family: 'Roboto', sans-serif;
